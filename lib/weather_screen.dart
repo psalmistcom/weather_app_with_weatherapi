9b3869767bfd359acb7ce1a08eb09ecb1839d1d2
psalmistcom <dadewale9@gmail.com>
2023-12-01 22:25:42 +0100

    added open weather api

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -3,9 +3,33 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:weather_app/additional_info_item.dart';
 import 'package:weather_app/hourly_forcast_item.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/secrets.dart';
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getCurrentWeather();
+  }
+
+  Future getCurrentWeather() async {
+    String cityName = "London";
+    final res = await http.get(
+      //create a file called secrets and add a const called "openWeatherAPIKey"
+      Uri.parse(
+          'http://api.openweathermap.org/data/2.5/weather?q=$cityName,uk&APPID=$openWeatherAPIKey'),
+    );
+
+    // print(res.body);
+  }
 
   @override
   Widget build(BuildContext context) {
