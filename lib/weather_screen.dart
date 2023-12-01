b7e461478dc4f5a37962a8bc1e5e58c9e847927d
psalmistcom <dadewale9@gmail.com>
2023-12-01 16:10:21 +0100

    appbar done

---
+++ lib/weather_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: const Text(
+          "Weather App",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.refresh,
+            ),
+          ),
+        ],
+      ),
+    );
+    // return const ;
+  }
+}
